440a3667d64b052def95b20d4a69bf93530dd036
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-02-05 18:08:02 +0800

    Update remit-records.php

--- remit-records/remit-records.php
+++ remit-records/remit-records.php
@@ -44,7 +44,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-active py-4 px-4">
         <div class="d-flex align-items-center">
             <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-            <h2 class="fs-2 m-0">Dashboard</h2>
+            <h2 class="fs-2 m-0">College Remitted Records</h2>
         </div>
     </nav>
     <div class="container-fluid px-4">
@@ -52,7 +52,6 @@
                     <div class="col-md-3">
                     <div class="col-md-3">
                         <div class="row my-5">
-                            <h3 class="fs-4 mb-3">College Remitted Records</h3>
                             <div class="col-md-3" id="topnav">
                                 <form action="Search">
                                 <input type="text" placeholder="Search.."></i>
