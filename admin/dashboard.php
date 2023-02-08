6a89ee1815fcd56e0b4973f7112a10fa12d0ef0b
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-02-08 14:23:16 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!--- links for bootstrap and css  --->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="../css/dashboard.css" />
@@ -14,7 +15,7 @@
 
       <body>
       <div class="d-flex" id="wrapper">
-        <!-- Sidebar -->
+        <!-- Sidebar with bootstrap -->
         <div class="bg-white" id="sidebar-wrapper">
             <img src="../images/logo.jpg" width ="200" alt="CCS COLLECTION FEE">
             <div class="list-group list-group-flush my-3">
@@ -30,6 +31,7 @@
             </div>
         </div>
   <div id="page-content-wrapper">
+<!-- Dashboard hamburger      -->
     <nav class="navbar navbar-expand-lg navbar-light bg-active py-4 px-4">
         <div class="d-flex align-items-center">
             <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
@@ -37,6 +39,7 @@
         </div>
     </nav>
   </div>
+<!-- Script for dashboard hamburger         -->
         </body>       
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
             <script>
