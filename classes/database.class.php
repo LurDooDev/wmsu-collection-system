8a86d7af436e499243987a84c9bd1e4756729f6a
LurDooDev <111673346+LurDooDev@users.noreply.github.com>
2023-02-10 13:21:04 +0800

    Update database.class.php

--- classes/database.class.php
+++ classes/database.class.php
@@ -2,9 +2,9 @@
 
 class Database{
     private $host = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $database = 'wmsucollection';
+    private $username = 'wmsu';
+    private $password = 'Godisgood420!';
+    private $database = 'u742250709_wmsucollection';
     protected $connection;
 
     function connect(){
