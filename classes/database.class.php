9e07882cff5adf1cb226593f54e99beec84d39d4
LurDooDev <111673346+LurDooDev@users.noreply.github.com>
2023-02-10 13:26:48 +0800

    Update database.class.php

--- classes/database.class.php
+++ classes/database.class.php
@@ -1,7 +1,7 @@
 <?php
 
 class Database{
-    private $host = 'localhost';
+    private $host = 'u742250709_';
     private $username = 'u742250709_wmsu';
     private $password = 'Godisgood420!';
     private $database = 'u742250709_wmsucollection';
