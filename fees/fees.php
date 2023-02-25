4cf4d486c51f527b714a61759a2654ab8fc9a8d3
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-02-25 09:45:03 +0800

    Add files via upload
    audit log and financial report not done

--- fees/fees.php
+++ fees/fees.php
@@ -37,16 +37,17 @@
         <!-- Sidebar with bootstrap -->
         <div class="bg-white" id="sidebar-wrapper">
             <img src="../images/logo.jpg" width ="200" alt="CCS COLLECTION FEE">
-            <div class="list-group list-group-flush my-3">
-                <a href="../admin/dashboard.php" class="list-group-item list-group-item-action bg-hover first-text fw">Dashboard</a>
-                <a href="../fees/fees.php" class="list-group-item list-group-item-action bg-hover first-text active">Fees</a>
-                <a href="../remit-records/remit-records.php" class="list-group-item list-group-item-action bg-hover first-text fw">Remit Records</a>
-                <a href="../college/college.php" class="list-group-item list-group-item-action bg-hover first-text fw">Colleges</a>
-                <a href="../funds/funds.php" class="list-group-item list-group-item-action bg-hover first-text fw">Funds</a>
-                <a href="../financial-report/financial-report.php" class="list-group-item list-group-item-action bg-hover first-text fw">Financial Report</a>
-                <a href="../audit-log/audit-log.php" class="list-group-item list-group-item-action bg-hover first-text fw">Audit Log</a>
-                <a href="../admin-settings/admin-settings.php" class="list-group-item list-group-item-action bg-hover first-text fw">Admin Settings</a>
-                <a href="../public/logout.php" class="list-group-item list-group-item-action bg-hover first-text fw">Logout</a>
+			<div class="list-group list-group-flush my-3">
+                <a href="../admin/dashboard.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Dashboard</a>
+                <a href="../fees/fees.php" class="list-group-item list-group-item-action bg-hover first-text  fw-bold active">Fees</a>
+                <a href="../payment/payment.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Payment</a>
+                <a href="../payment-records/payment-records.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Payment Records</a>
+                <a href="../students/students.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Students</a>
+                <a href="../financial-report/financial-report.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Financial Report</a>
+                <a href="../audit-log/audit-log.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Audit Log</a>
+                <a href="../csc-management/csc-management.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">CSC Management</a>
+                <a href="../admin-settings/admin-settings.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Admin Settings</a>
+                <a href="../public/logout.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Logout</a>
             </div>
         </div>
 		<div class="table-responsive">
@@ -61,10 +62,17 @@
 		<div class="table-wrapper">
 		<div class="table-title">
 				<div class="row">
-					<div class="col-sm-6">
+					<div class="col-sm-4">
+					<input class="form-control border" type="search" name= "search" id="search-input" placeholder="Search Name">
+					<button class="btn btn-primary dropdown-toggle" id ="sort-by" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sort By </button>
+						<div class="dropdown-menu">
+    					<a class="dropdown-item" href="#">Ascending</a>
+    					<a class="dropdown-item" href="#">Descending</a>
 					</div>
-					<div class="col-sm-6">
-						<a href="#addFeesModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Fees</span></a>
+					</div>
+					<div class="col-sm-8">
+						<a href="#addFeesModal" class="btn btn-success" id = "add-fees" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Fees</span></a>
+
 						<!-- <a href="#deleteFeesModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>						 -->
 					</div>
 				</div>
@@ -83,14 +91,15 @@
 						<th>Description</th>
 						<th>Amount</th>
 						<th>Duration</th>
+						<th>School Year</th>
 						<th>Action</th>
 					</tr>
 				</thead>
 				<tbody>
 				<?php
-					$fee = new Fee();
-					$data = $fee->show();
-				foreach($data as $fee) {
+					//$fee = new Fee();
+					//$data = $fee->show();
+				//foreach($data as $fee) {
         ?>
 					<tr>
 						<!-- <td>
@@ -99,20 +108,44 @@
 								<label for="checkbox1"></label>
 							</span>
 						</td> -->
-						<td><?php echo $fee['fee_id']; ?></td>
-						<td><?php echo $fee['fee_type']; ?></td>
-						<td><?php echo $fee['fee_description']; ?></td>
-						<td><?php echo $fee['fee_amount']; ?></td>
-						<td><?php echo $fee['fee_due_date']; ?></td>
+						<td>1</td>
+           			 <td>CCS</td>
+            		<td>CCS Festival</td>
+            		<td>200</td>
+            		<td>1st Semester</td>
+            		<td>05/19/2022</td>
+					<td>
+					<a href="#editFeesModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+							<a href="#deleteFeesModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+							</td>
+				</tr>
+							<td>2</td>
+           			 <td>CCS</td>
+            		<td>CCS Rambulan</td>
+            		<td>150</td>
+            		<td>1st Semester</td>
+            		<td>05/20/2022</td>
+					<td>
+					<a href="#editFeesModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+							<a href="#deleteFeesModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+							</td>
+					</tr>
+					</tbody>
+			</table>
+						<!--<td><?php //echo $fee['fee_id']; ?></td>
+						<td><?php  // echo $fee['fee_type']; ?></td>
+						<td><?php //echo $fee['fee_description']; ?></td>
+						<td><?php //echo $fee['fee_amount']; ?></td>
+						<td><?php //echo $fee['fee_due_date']; ?></td>
 						<td>
 							<a href="#editFeesModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
 							<a href="#deleteFeesModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
 						</td>
 					</tr>
-					<?php } ?>
+					<?php //} ?>
 				</tbody>
 			</table>
-			<!-- <div class="clearfix">
+			<- - <div class="clearfix">
 				<div class="hint-text">Showing <b>1</b> out of <b>25</b> entries</div>
 				<ul class="pagination">
 					<li class="page-item disabled"><a href="#">Previous</a></li>
@@ -152,7 +185,16 @@
 					<div class="form-group">
 						<label for="feeAmount">Amount</label>
 						<input type="number" name="feeAmount" id="feeAmount" class="form-control" required>
-					</div>					
+					</div>
+					<div class="form-group">
+						<label for ="">School Year</label> 
+						<input type="text" name ="" id= "" class="form-control" required> <!-- text muna kasi automatically default - mark -->
+					</div>	
+					<div class="form-group">
+						<label for = "">Semester</label>
+						<input type="text" name = " " id= " " class="form-control" required><!-- text muna kasi automatically default - mark -->
+					</div>						
+
 				</div>
 				<div class="modal-footer">
 					<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
@@ -188,7 +230,16 @@
 					<div class="form-group">
 					<label>Amount</label>
 						<input type="number" class="form-control" required>
-					</div>					
+					</div>		
+					<div class="form-group">
+						<label for ="">School Year</label> 
+						<input type="text" name ="" id= "" class="form-control" required> <!-- text muna kasi automatically default - mark -->
+					</div>	
+					<div class="form-group">
+						<label for = "">Semester</label>
+						<input type="text" name = " " id= " " class="form-control" required><!-- text muna kasi automatically default - mark -->
+					</div>						
+			
 				</div>
 				<div class="modal-footer">
 					<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
