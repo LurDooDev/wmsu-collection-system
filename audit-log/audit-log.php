b01a03e92b08ab578a4503c367fa1872584e3087
AndreMickael <111483607+AndreMickael@users.noreply.github.com>
2023-03-05 17:27:38 +0800

    Update audit-log.php

--- audit-log/audit-log.php
+++ audit-log/audit-log.php
@@ -27,7 +27,7 @@ if (!isset($_SESSION['logged_id'])){
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../css/funds.css" />
+    <link rel="stylesheet" href="../css/audit.css" />
     <link rel="stylesheet" href="../css/payments.css" />
     <link rel="stylesheet" href="../css/dashboard.css" />
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -53,18 +53,65 @@ if (!isset($_SESSION['logged_id'])){
                 <a href="../public/logout.php" class="list-group-item list-group-item-action bg-hover fw-bold">Logout</a>
             </div>
         </div>
-		<div class="table-responsive">
+	
+        <div class="table-responsive">
 	<div id="page-content-wrapper">
 <!-- Dashboard hamburger      -->
     <nav class="navbar navbar-expand-lg navbar-light bg-active py-4 px-4">
-            <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
+        <div class="d-flex align-items-center">
             <h2 class="fs-2 m-0" style="color:#000000; font-weight: 400;">Audit Log</h2>
         </div>
     </nav>
     <div class="container">
-		<div class="row justify-content-center">
-
-  
+    <div class="col-sm-14" id="lakatan">
+                <table class="table table-striped table-borderless">
+            <thead style="background-color:#95BDFE ;" class="text-white">
+              <tr>
+                <hr style="height: 6px; color: #FFF;">
+                <th scope="col" style = " color: #000000;" >ID</th>
+                <th scope="col" style = " color: #000000;" >Name</th>
+                <th scope="col" style = " color: #000000;" >Officer</th>
+                <th scope="col" style = " color: #000000;" >Date</th>
+                <th scope="col" style = " color: #000000;" >Time</th>
+                <th scope="col" style = " color: #000000;" >Action Made</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Joy Cubile</td>
+                <td>CCS Mayor</td>
+                <td>12/13/2022</td>
+                <td>14:20</td>
+                <td>Collected Payment CSC Fee of Arthur Nery</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jerome Rabara</td>
+                <td>USC President</td>
+                <td>12/15/2022</td>
+                <td>17:20</td>
+                <td>Added User for CCS Treasurer name Aj Roblox</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Joy Cubile</td>
+                <td>CCS Mayor</td>
+                <td>12/14/2022</td>
+                <td>15:20</td>
+                <td>Collected Payment CSC Fee of Eminem Pinoy</td>
+              </tr>
+              <td>4</td>
+                <td>Joy Cubile</td>
+                <td>CCS Mayor</td>
+                <td>12/14/2022</td>
+                <td>15:20</td>
+                <td>Collected Payment CSC Fee of Lil Pumpskie</td>
+              </tr>
+              
+          </table>
+                </div>
+                </div>
 </body>       
 
 <!-- Script for dashboard hamburger         -->
