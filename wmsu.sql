d8d820392f5886894b7bc76e58273db09cd26329
LurDooDev <devbryansevilla@gmail.com>
2023-03-12 04:31:45 +0800

    database update

--- wmsu.sql
+++ wmsu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2023 at 07:42 PM
+-- Generation Time: Mar 11, 2023 at 09:31 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -41,6 +41,30 @@ INSERT INTO `colleges` (`id`, `college_name`, `college_code`) VALUES
 (1, 'College of Computing Studies', 'CCS'),
 (2, 'College of Engineering', 'COE'),
 (3, 'College of Nursing', 'CN');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fee_options`
+--
+
+CREATE TABLE `fee_options` (
+  `id` int(11) NOT NULL,
+  `option_name` varchar(50) NOT NULL,
+  `option_value` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `programs`
+--
+
+CREATE TABLE `programs` (
+  `id` int(11) NOT NULL,
+  `program_name` varchar(50) NOT NULL,
+  `college_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -102,6 +126,22 @@ INSERT INTO `semesters` (`id`, `semester_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `student_email` varchar(50) NOT NULL,
+  `year_level` varchar(20) NOT NULL,
+  `college_id` int(11) NOT NULL,
+  `program_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `university_fee`
 --
 
@@ -142,8 +182,9 @@ CREATE TABLE `university_fee_schedule` (
 --
 
 INSERT INTO `university_fee_schedule` (`id`, `university_fee_id`, `semester_id`, `school_year_id`, `university_start_date`, `university_end_date`, `created_by`) VALUES
-(1, 1, 1, 1, '2023-03-11', '2023-04-20', 'Bryan'),
-(2, 2, 2, 1, '2023-03-17', '2023-03-30', 'Bryan');
+(3, 2, 1, 1, '2023-03-24', '2023-03-29', 'Bryan'),
+(4, 1, 1, 1, '2023-03-15', '2023-03-23', 'Bryan'),
+(5, 2, 1, 1, '2023-03-16', '2023-03-22', 'Bryan');
 
 -- --------------------------------------------------------
 
@@ -166,7 +207,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `user_username`, `user_fullname`, `user_position`, `user_password`, `role_id`, `college_id`) VALUES
-(1, 'adMin101', 'Bryan', 'President', '$2y$10$9lSPgrM/5rCbXrHZz1s6R.atAJGxcnq4ffRt5ecpCFYh6cj3HTeTK', 1, 1);
+(1, 'adMin101', 'Bryan', 'President', '$2y$10$9lSPgrM/5rCbXrHZz1s6R.atAJGxcnq4ffRt5ecpCFYh6cj3HTeTK', 1, 1),
+(2, 'offiCER101', 'Bryan The Officer', 'Mayor', '$2y$10$gRTuUjQvgmTOMaLlI4aljOEdGXsBNlW1F3mAmTFvVzKrK6PkJJx9G', 2, 1);
 
 --
 -- Indexes for dumped tables
@@ -177,6 +219,19 @@ INSERT INTO `users` (`id`, `user_username`, `user_fullname`, `user_position`, `u
 --
 ALTER TABLE `colleges`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `fee_options`
+--
+ALTER TABLE `fee_options`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `programs`
+--
+ALTER TABLE `programs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `college_id` (`college_id`);
 
 --
 -- Indexes for table `roles`
@@ -195,6 +250,14 @@ ALTER TABLE `school_year`
 --
 ALTER TABLE `semesters`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `college_id` (`college_id`),
+  ADD KEY `program_id` (`program_id`);
 
 --
 -- Indexes for table `university_fee`
@@ -230,6 +293,18 @@ ALTER TABLE `colleges`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `fee_options`
+--
+ALTER TABLE `fee_options`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `programs`
+--
+ALTER TABLE `programs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
@@ -257,17 +332,30 @@ ALTER TABLE `university_fee`
 -- AUTO_INCREMENT for table `university_fee_schedule`
 --
 ALTER TABLE `university_fee_schedule`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `programs`
+--
+ALTER TABLE `programs`
+  ADD CONSTRAINT `programs_ibfk_1` FOREIGN KEY (`college_id`) REFERENCES `colleges` (`id`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`college_id`) REFERENCES `colleges` (`id`),
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`program_id`) REFERENCES `programs` (`id`);
 
 --
 -- Constraints for table `university_fee_schedule`
