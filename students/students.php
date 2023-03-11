8d9ea71311b65fe2762a253b166446d311056b5d
LurDooDev <devbryansevilla@gmail.com>
2023-03-12 02:47:37 +0800

    kulang lang div?

--- students/students.php
+++ students/students.php
@@ -184,6 +184,7 @@ require_once '../classes/college.class.php';
 		</div>
 	</div>
 </div>
+				 </div>
 </body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
             <script>
