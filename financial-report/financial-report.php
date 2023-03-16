a659c74fde4fa582316b3e143f007fcac3539559
AndreMickael <111483607+AndreMickael@users.noreply.github.com>
2023-03-16 12:46:31 +0800

    Update financial-report.php

--- financial-report/financial-report.php
+++ financial-report/financial-report.php
@@ -34,6 +34,7 @@ if (!isset($_SESSION['logged_id'])) {
     <link rel="stylesheet" href="../css/payments.css" />
     <link rel="stylesheet" href="../css/dashboard.css" />
     <link rel="stylesheet" href="../css/financial.css" />
+    <link rel="stylesheet" href="../css/user-college.css" />
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
@@ -76,7 +77,7 @@ if (!isset($_SESSION['logged_id'])) {
                 <table class="table">
 				<div class="row">
                 <div class="ml-auto p-auto" style="display: flex; align-items: center; justify-content: flex-end; padding: 15px;">
-                <a href="" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Generated Report</span></a>
+                <a href="report.php" class="btn btn-success"> <i class="material-icons">&#xE147;</i> <span>Generated Report</span></a>
                         <div class="ml-7 p-auto" style="display: flex; align-items: center; justify-content: flex-end; padding: 15px;">
 						<a href="#addReport" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add Report</span></a>
 </div>
@@ -198,6 +199,7 @@ if (!isset($_SESSION['logged_id'])) {
         </div>
     </div>
 </div>
+
 <!-- Script for dashboard hamburger         -->
         </body>       
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
@@ -209,3 +211,4 @@ if (!isset($_SESSION['logged_id'])) {
                     el.classList.toggle("toggled");
                 };
             </script>
+
