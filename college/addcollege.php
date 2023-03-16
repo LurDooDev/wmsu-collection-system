1ebc612bff5831520b90b2f01e0b4ec9fc075edd
dz1an <114967654+dz1an@users.noreply.github.com>
2023-03-16 20:43:23 +0800

    Update addcollege.php

--- college/addcollege.php
+++ college/addcollege.php
@@ -5,13 +5,34 @@ require_once "../classes/college.class.php";
 // Check if the form has been submitted
 if (isset($_POST['action']) && $_POST['action'] == 'add') {
     
-    $code = htmlspecialchars($_POST['code']);
-    $name = htmlspecialchars($_POST['name']);
-    
+    // Sanitize input data
+    $code = filter_input(INPUT_POST, 'code', FILTER_SANITIZE_STRING);
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+
+    // Check if any of the form fields are empty
+    if (empty($code) || empty($name)) {
+        echo 'Error: Please fill in all required fields.';
+        exit();
+    }
+
+    // Validate the college code format (should be alphanumeric with length between 1 and 10 characters)
+    if (!preg_match('/^[a-zA-Z0-9]{1,10}$/', $code)) {
+        echo 'Error: College code must be alphanumeric and between 1 and 10 characters.';
+        exit();
+    }
+
     $College = new College();
+
+    // Check if the code is already in use
+    $existingCollege = $college->getCollegeByCode($code);
+    if ($existingCollege) {
+        echo 'Error: College code "' . $code . '" is already in use by "' . $existingCollege->collegeName . '".';
+        exit();
+    }
+
     $College->collegeCode = $code;
     $College->collegeName = $name;
-    
+
     if ($College->createCollege()) {
         header('location: college.php');
     } else {
@@ -19,6 +40,3 @@ if (isset($_POST['action']) && $_POST['action'] == 'add') {
     }
     
 }
-
-
-?>
