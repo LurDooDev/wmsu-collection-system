e8e412d3f4a1fde0b2622a25b2a4445ca4502f68
dz1an <114967654+dz1an@users.noreply.github.com>
2023-03-16 23:52:07 +0800

    Update college.php
    only officer can access else go to dashboard
    also improve validation

--- college/college.php
+++ college/college.php
@@ -1,18 +1,21 @@
 <?php
-    session_start();
-	require_once '../functions/session.function.php';
+   session_start();
+require_once '../functions/session.function.php';
 
-	//prevent unauthorized access
-	if (!isset($_SESSION['logged_id'])) {
-		header('location: ../public/logout.php');
-	} else if ($_SESSION['role'] != 'officer') {
-		header('location: ../admin/dashboard-main.php');
-	}
+// Prevent unauthorized access
+if (!isset($_SESSION['logged_id']) || empty($_SESSION['logged_id']) || !isset($_SESSION['role']) || empty($_SESSION['role'])) {
+    // If user is not logged in or session variables are empty
+    header('location: ../public/logout.php');
+} else if ($_SESSION['role'] != 'officer') {
+    // If user is not an officer
+    header('location: ../admin/dashboard-main.php');
+    exit(); // Terminate script to prevent further execution
+}
 
-	// Only officers can see this page
-	require_once '../classes/database.class.php';
-	require_once '../classes/college.class.php';
-	require_once '../classes/program.class.php';
+// Only officers can see this page
+require_once '../classes/database.class.php';
+require_once '../classes/college.class.php';
+require_once '../classes/program.class.php';
 ?>
 <!doctype html>
 <html lang="en" class="no-js">
