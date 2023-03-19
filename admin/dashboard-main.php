9e7d336ccd60dab80d479f579820a6a0ef024461
AndreMickael <mollycupcake63@gmail.com>
2023-03-19 19:30:05 +0800

    test

--- admin/dashboard-main.php
+++ admin/dashboard-main.php
@@ -44,9 +44,7 @@
         <div class="bg-white" id="sidebar-wrapper">
             <img src="../images/logo.jpg" width ="200" alt="CCS COLLECTION FEE">
             <div class="list-group list-group-flush my-3">
-            
                 <a href="../admin/dashboard-main.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold active">Dashboard</a>
-                
                 <!-- <a href="../fees/fees.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Fees</a> -->
                 <a href="../remit-records/remit-records.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold ">Remit Records</a>
                 <a href="../college/college.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Colleges</a>
@@ -60,7 +58,7 @@
                 <a href="../financial-report/financial-report.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Financial Report</a>
                 <a href="../audit-log/audit-log.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Audit Log</a>
                 <button class="list-group-item list-group-item-action bg-hover second-text dropdown-btn fw-bold">Admin Settings
-                <i class="fa fa-caret-down" style="margin-left: 44px;"></i>
+                <i class="fa fa-caret-down" style="margin-left: 45px;"></i>
                 </button>
                 <div class="dropdown-container">
                     <a href="../admin-settings/overview_settings.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold" style="text-decoration:none; padding-left: 70px;">Overview</a>
