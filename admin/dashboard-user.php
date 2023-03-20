7a10510b25ce6b14ddadc1e876d9d4712d9d2bdd
AndreMickael <111483607+AndreMickael@users.noreply.github.com>
2023-03-20 16:45:16 +0800

    Update dashboard-user.php

--- admin/dashboard-user.php
+++ admin/dashboard-user.php
@@ -1,19 +1,19 @@
 <?php
-    session_start();
-	require_once '../functions/session.function.php';
+// resume session here to fetch session values
+session_start();
 
-	//prevent unauthorized access
-	if (!isset($_SESSION['logged_id'])) {
-		header('location: ../admin/dasboard-main.php');
-		exit(); // stop execution after redirect
-	} else if ($_SESSION['role'] != 'admin' && $_SESSION['role'] != 'officer') {
-		header('location: ../admin/dashboard-main.php');
-		exit(); // stop execution after redirect
-	}
+//prevent horny people
+if (!isset($_SESSION['logged_id'])) {
+  header('location: ../public/logout.php');
+} else if ($_SESSION['role'] != 'officer') {
+  if ($_SESSION['role'] == 'admin') {
+      header('location: dashboard.php');
+  } else if ($_SESSION['role'] == 'collector') {
+      header('location: dashboard-user.php');
+  }
+}
 
-	require_once '../classes/database.class.php';
-	require_once '../classes/college.class.php';
-	?>
+?>
 <!doctype html>
 <html lang="en" class="no-js">
   <html>
@@ -23,7 +23,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <!--- links for bootstrap and css  --->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="../css/payment.css" />
     <!-- Unicons CSS -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
@@ -45,15 +44,21 @@
         <div class="bg-white" id="sidebar-wrapper">
             <img src="../images/logo.jpg" width ="200" alt="CCS COLLECTION FEE">
             <div class="list-group list-group-flush my-3">
-                <a href="../admin/dashboard-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold active">Dashboard</a>
+                <a href="../admin/dashboard-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold active ">Dashboard</a>
                 <a href="../fees-user/fees-user.php" class="list-group-item list-group-item-action bg-hover first-text  fw-bold ">Fees</a>
-                <a href="../payment/payment.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Payment</a>
+                <button class="list-group-item list-group-item-action bg-hover second-text dropdown-btn fw-bold">Payments</a>
+                <i class="fa fa-caret-down" style = "margin-left:18px;"></i>
+                </button>                
+                <div class="dropdown-container">
+                    <a href="../payment/universitypayment.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold" style="text-decoration:none; padding-left: 70px;">University Payment</a>
+                    <a href="../payment/localpayment.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold " style="text-decoration:none; padding-left: 70px;">Local Payment</a>
+                </div>
                 <a href="../payment-records/payment-records.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Payment Records</a>
-                <a href="../students/students.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Students</a>
+                <a href="../students/students.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold ">Students</a>
                 <a href="../financial-report-user/financial-report-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Financial Report</a>
                 <a href="../audit-log/audit-log.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Audit Log</a>
                 <a href="../csc-management/csc-management.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">CSC Management</a>
-                <a href="../admin-settings-user/admin-settings-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Admin Settings</a>
+                <a href="../admin-settings/admin-settings.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Admin Settings</a>
                 <a href="../public/logout.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Logout</a>
             </div>
         </div>
@@ -61,18 +66,12 @@
 	<div id="page-content-wrapper">
 <!-- Dashboard hamburger      -->
     <nav class="navbar navbar-expand-lg navbar-light bg-active py-4 px-4">
+        <div class="d-flex align-items-center">
             <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-            <h2 class="fs-2 m-0" style="color: #000000;">Dashboard</h2>
+            <h2 class="fs-2 m-0" style="color:#000000; font-weight: 400;">Dashboard</h2>
         </div>
     </nav>
-  </div>
-
-<!-- New content here  -->
-
-
-
-<!-- Script for dashboard hamburger         -->
-        </body>       
+</body>       
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
             <script>
                 var el = document.getElementById("wrapper");
@@ -82,3 +81,22 @@
                     el.classList.toggle("toggled");
                 };
             </script>
+
+</html>
+<script>
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+</script>
