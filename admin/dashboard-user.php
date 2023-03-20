310948fef6d8c4b1560ccb5b5904f6b661e8d04d
AndreMickael <111483607+AndreMickael@users.noreply.github.com>
2023-03-20 17:16:25 +0800

    Update dashboard-user.php

--- admin/dashboard-user.php
+++ admin/dashboard-user.php
@@ -30,7 +30,6 @@ if (!isset($_SESSION['logged_id'])) {
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../css/payments.css" />
     <link rel="stylesheet" href="../css/dashboard.css" />
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
