785441c9631223f703e5049c347cf236aa22c1aa
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-03-27 04:55:09 +0800

    Revert "dropdown scirpt"
    This reverts commit e80128644e2895bb55e1fb96403626c1848f7e91.

--- funds/overview_funds.php
+++ funds/overview_funds.php
@@ -277,22 +277,4 @@ for (var i = 0; i < links.length; i++) {
     setActiveLink(this);
   });
 }</script>
-<script>
-	$(document).ready(function() {
-		// Hide the dropdown on page load
-		$('.dropdown-container').hide();
-
-		// Toggle the dropdown on click
-		$('.dropdown-btn').click(function() {
-			var containerId = $(this).attr('data-container');
-			$('#'+containerId).toggle();
-		});
-
-		// Add the active class on click
-		$('.list-group-item-action').click(function() {
-			$('.list-group-item-action').removeClass('active');
-			$(this).addClass('active');
-		});
-	});
-</script>
 </html>
