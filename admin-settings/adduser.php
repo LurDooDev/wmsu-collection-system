1661e6a2c2f0e2f68eedc8d61c2d8d49dfcf1bce
kent <xt202001241@wmsu.edu.ph>
2023-03-28 02:12:55 +0800

    validation updated but database not yet connected

--- admin-settings/adduser.php
+++ admin-settings/adduser.php
@@ -17,42 +17,50 @@ if (isset($_POST['action']) && $_POST['action'] == 'add') {
     $users->userpassword = $hashed_password;
 
     // Validate form fields
-    $errors = array();
+    $error = array();
     
     if (empty($users->username) || empty($users->userfullname) || empty($users->userposition) || empty($users->roleID) || empty($users->collegeID) || empty($password)) {
-        $errors[] = 'Please fill in all the required fields';
+        $error = true; // Set the flag variable to true
+        echo '<script>alert("Please fill in all the required fields");</script>';
+        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
+        return;
     }
     
-    if (strlen($users->username) > 50 || strlen($users->userfullname) > 100 || strlen($users->userposition) > 50 || strlen($password) > 50) {
-        $errors[] = 'Field length exceeds the limit';
+    else if (strlen($users->username) > 50 || strlen($users->userfullname) > 100 || strlen($users->userposition) > 50 || strlen($password) > 50) {
+        $error = true; // Set the flag variable to true
+        echo '<script>alert("Field length exceeds the limit");</script>';
+        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
+        return;
     }
     
-    if (!filter_var($users->username, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Invalid email format';
-    }
-    
-    if (!is_numeric($users->roleID) || !is_numeric($users->collegeID)) {
-        $errors[] = 'Invalid input';
+    else if (!is_numeric($users->roleID) || !is_numeric($users->collegeID)) {
+        $error = true; // Set the flag variable to true
+        echo '<script>alert("Invalid input");</script>';
+        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
+        return;
     }
 
-    if (strlen($password) < 6) {
-        $errors[] = 'Password must be at least 6 characters long';
+    else if (strlen($password) < 6) {
+        $error = true; // Set the flag variable to true
+        echo '<script>alert("Password must be at least 6 characters long");</script>';
+        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
+        return;
     }
     
-    if (!empty($errors)) {
-        echo 'Error: ' . implode(', ', $errors);
-        header('Location: ../admin-settings/user.php');
+    else if (!empty($error)) {
+        echo 'Error: ' . implode(', ', $error);
+        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
         exit;
     }
 
     // Add user to database
     if ($users->addUser()) {
         echo 'Success';
-        header('Location: ../admin-settings/user.php');
+        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
         exit;
     } else {
         echo 'Error: Unable to add user';
-        header('Location: ../admin-settings/user.php');
+        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
         exit;
     }
 }
