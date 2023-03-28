390eca161deaf0bd0176dfbcfd1e257235c23bc6
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-03-28 21:29:16 +0800

    test

--- students/student-details.php
+++ students/student-details.php
@@ -31,7 +31,8 @@ if (!isset($_SESSION['logged_id'])) {
 	  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/payments.css" />
-    <link rel="stylesheet" href="../css/dashboard.css" />
+    <link rel="stylesheet" href="../css/paymentlocal.css" />
+    <script src="https://kit.fontawesome.com/6023332cf2.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
     </head>
       <body>
@@ -63,10 +64,6 @@ if (!isset($_SESSION['logged_id'])) {
                             <span class="text-sm text-grey text-black align-middle">3rd Year</span>
                          </div>
                         <div class="text-grey-m2">
-                            <div class="my-1" >
-                            <span class="">Type of Payment:</span>
-                            <span class="">University</span>
-                            </div>
                             <div class="my-1">
                                Status: Enrolled
                          </div>
@@ -108,7 +105,8 @@ if (!isset($_SESSION['logged_id'])) {
                 <td style="text-align:center; color: red;"> Unpaid</td>
                 <td style="text-align:center">₱ 200</td>
                 <td style="text-align:center">PHP 0.00</td>
-                <td style="text-align:center"><button  class="view btn btn-outiline-dark btn-sm" data-bs-toggle="modal" data-bs-target="#paymentdetails"><i class="material-icons" ></i></button></td>
+                <td style="text-align:center"><button class="view btn btn-outiline-dark btn-sm" data-bs-toggle="modal" data-bs-target="#paymentdetails"><i class="material-icons">receipt</i></button>
+              </td>
               </tr>
               <tr>
                 <td style="text-align:center"> Local Fees</td>
@@ -118,9 +116,7 @@ if (!isset($_SESSION['logged_id'])) {
                 <td style="text-align:center; color: red;"> Paid</td>
                 <td style="text-align:center">₱ 200</td>
                 <td style="text-align:center">PHP 200.00</td>
-                <td style="text-align:center"> <a href="#receipt" class="view-schedules" data-toggle="modal">
-                    <i class="material-icons" data-toggle="tooltip" title="View Receipt">receipt</i>
-                </a>
+                <td style="text-align:center"> <button class="view btn btn-outiline-dark btn-sm" data-bs-toggle="modal" data-bs-target="#paymentdetails"><i class="material-icons">receipt</i></button>
         </td>
               </tr>
     
@@ -130,62 +126,97 @@ if (!isset($_SESSION['logged_id'])) {
 <div class="modal fade modal-fullscreen" id="paymentdetails" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-  <div class="modal-header">
-<h5 class="modal-title" id="exampleModalLabel">College of Computing Studies(CCS)</h5>
-        <div>
-        <a data-toggle="modal" href="#AddCSV" class="btn btn-primary">Add CSV</a>
-</div>
+  <div class="text-center">
+<h5 class="modal-title" id="exampleModalLabel" style="font-size: 30px; margin-top: 20px">College of Computing Studies(CCS)</h5>
 </div>
         <div class="modal-body">
-      <table class="table table-striped">
-            <thead style="background-color:#95BDFE ;" class="text-white">
-            <tr>
-                <th scope="col" style = " color: #000000;" >Student ID</th>
-                <th scope="col" style = " color: #000000;" >Student Name</th>
-                <th scope="col" style = " color: #000000;" >Student Email</th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>2020-1311</td>
-                <td>Joy Cubile</td>
-                <td>xt202001311@wmsu.edu.ph</td>
-              </tr>
+        <div class="table-responsive">
+          <!-- content here -->
+    <div class="row">
+                    <div class="col-sm-6">
+                        <div>
+                            <span class="text-sm text-grey-m2 align-middle">Name:</span>
+                            <span class="text-600 text-110 text-black align-middle">Bryan Christian Sevilla</span>
+                        </div>
+                        <div class="my-1">
+                            <span class="text-sm text-grey-m2 align-middle">Email:</span>
+                            <span class="text-sm text-grey text-black align-middle">sl201503664@wmsu.edu.ph</span>
+                         </div>
+                        <div class="my-1">
+                            <span class="text-sm text-grey-m2 align-middle">Year Level:</span>
+                            <span class="text-sm text-grey text-black align-middle">3rd Year</span>
+                         </div>
+                    </div>
+                    <!-- /.col -->
 
-              <tr>
-                <td>2020-1294</td>
-                <td>John Kent Tingkasan</td>
-                <td>xt202001311@wmsu.edu.ph</td>
-              </tr>
+                    <div class="text-95 col-sm-6 align-self-start d-sm-flex justify-content-end">
+                        <div class="text-grey-m2">
+                        <img src="../images/ccs.jpg" width ="100" alt="CCS COLLECTION FEE" style="margin-top: 0px; padding-bottom: 10px;">
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <div class="table-responsive" id="yati">
+                <table style="width: 100%; border: 2px solid black; border-collapse: collapse;">
+  <thead style="color: grey; background-color: black;">
+    <tr>
+      <th style="text-align: center; padding: 5px; border-right: 2px solid white;">No.</th>
+      <th style="text-align: center; padding: 5px; border-right: 2px solid white;">Description</th>
+      <th style="text-align: center; padding: 5px; border-right: 2px solid white;">Date</th>
+      <th style="text-align: center; padding: 5px;">Amount</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td style="text-align: center; padding: 5px; border-right: 1px solid black;">1</td>
+      <td style="text-align: center; padding: 5px; border-right: 1px solid black;">WMSU Boracay</td>
+      <td style="text-align: center; padding: 5px; border-right: 1px solid black;">11-02-222</td>
+      <td style="text-align: center; padding: 5px;">₱ 320</td>
+    </tr>
+    <tr>
+      <td style="text-align: center; padding: 5px; border-right: 1px solid black;">2</td>
+      <td style="text-align: center; padding: 5px; border-right: 1px solid black;">WMSU Fishing</td>
+      <td style="text-align: center; padding: 5px; border-right: 1px solid black;">11-03-222</td>
+      <td style="text-align: center; padding: 5px;">₱ 100</td>
+    </tr>
+    <tr>
+      <td style="text-align: center; padding: 5px; border-right: 1px solid black;">3</td>
+      <td style="text-align: center; padding: 5px; border-right: 1px solid black;">WMSU Rides</td>
+      <td style="text-align: center; padding: 5px; border-right: 1px solid black;">11-04-222</td>
+      <td style="text-align: center; padding: 5px;">₱ 100</td>
+    </tr>
+    <tr>
+      <td style="text-align: right; padding: 5px; border-right: 1px solid black;"></td>
+      <td style="text-align: right; padding: 5px; border-right: 1px solid black;"></td>
+      <td style="text-align: right; padding: 5px; border-right: 1px solid black;">Total Amount:</td>
+      <td style="text-align: center; padding: 5px;">₱ 520</td>
+    </tr>
   </tbody>
 </table>
 </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+<br>
+<br>
+  <div class="row">
+    <div class="col-sm-6">
+      <div>
+        <span class="text-sm text-110 text-black align-middle">Payment Method:</span>
+        <span class="text-600 text-110 text-black align-middle">Cash</span>
       </div>
+      <br>
+      <span class="text-sm text-110 text-black align-middle">Collected By:</span>
+      <span class="text-600 text-110 text-black align-middle">Eljen Mae Augusto</span>
     </div>
+    <div class="col-sm-6 d-flex flex-column align-items-end align-content-stretch" style="flex: 0 0 48%;">
+    <div class="d-flex justify-content-end align-items-center">
+    <button class="btn bg-white btn-light mx-1px text-95" style="border: 1px solid black;" href="#" data-title="Edit">
+      <i class="ml-auto fa-solid fa-pen-to-square text-primary-m1 text-120 w-2"></i> Edit
+    </button>
+    <button class="btn bg-white btn-light mx-1px text-95" style="border: 1px solid black;" href="#" data-title="Print">
+      <i class="ml-auto fa fa-print text-primary-m1 text-120 w-2"></i> Print
+    </button>
   </div>
 </div>
 </div>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-</body>       
+  
 
