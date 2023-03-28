b48b2f155b7d91927d73e2f2b6e1d32765adab19
AndreMickael <111483607+AndreMickael@users.noreply.github.com>
2023-03-29 06:13:30 +0800

    Update local-details.php

--- payment-local/local-details.php
+++ payment-local/local-details.php
@@ -132,7 +132,7 @@ require_once '../classes/student.class.php';
 
                     <div class="text-105 col-sm-6 align-self-start d-sm-flex justify-content-end">
                         <hr class="d-sm-none" />
-                        <div class="text-black-m5">
+                        <div class="text-black-m5" style = "margin-right:75px;">
                             <div class="my-2 text-bigger"> <span class="text-600 text-100">ID:</span> <?php echo $student['id']; ?></div>
                             <div class="my-2 text-bigger "> <span class="text-600 text-100">College:</span> <?php echo $student['college_code']; ?></div>
                             <div class="my-2 text-bigger"> <span class="text-600 text-100">Course:</span> <?php echo $student['program_name']; ?></div>
