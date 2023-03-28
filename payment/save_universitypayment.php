b2212951d86bc979916f3eab584acd183567bbdc
LurDooDev <devbryansevilla@gmail.com>
2023-03-28 22:29:31 +0800

    try

--- payment/save_universitypayment.php
+++ payment/save_universitypayment.php
@@ -1,46 +1,60 @@
 <?php
-  require_once '../classes/universitypayment.class.php';
-  require_once '../classes/student.class.php';
-  require_once '../classes/universityfeeSched.class.php';
-  session_start();
-  require_once '../functions/session.function.php';
+require_once '../classes/universitypayment.class.php';
+require_once '../classes/student.class.php';
+require_once '../classes/universityfeeSched.class.php';
+session_start();
+require_once '../functions/session.function.php';
 
+if(isset($_POST['payment_amount'])) {
+    $paymentAmount = $_POST['payment_amount'];
+} else {
+    $paymentAmount = 0;
+}
 
-  $payment = new UniversityPayment();
-  $studentID = $_GET['studentID'];
-  $feeScheduleID = $_GET['universityID'];
-  
-  // Retrieve student name and college from database using student ID
-  if (isset($_GET['studentID'])) {
+$payment = new UniversityPayment();
+$studentID = $_GET['studentID'];
+$feeScheduleID = $_GET['universityID'];
+
+// Retrieve student name and college from database using student ID
+if (isset($_GET['studentID'])) {
     $studentId = $_GET['studentID'];
     $student = new Student();
     $studentData = $student->showAllDetailsBystudentId($studentId); 
     foreach($studentData as $student) {        
-  $studentName = $student['first_name'] . ' ' . $student['last_name'];
-  $college = $student['college_name'];
-  $program = $student['program_name'];
-  $year_level = $student['year_level'];
+        $studentName = $student['first_name'] . ' ' . $student['last_name'];
+        $college = $student['college_name'];
+        $program = $student['program_name'];
+        $year_level = $student['year_level'];
     }
-  } else {
-    echo 'student dili jud';
-  }
-  
-  // Retrieve fee amount from database using fee schedule ID
-  if (isset($_GET['universityID'])) {
+} else {
+    echo 'Student not found.';
+    exit;
+}
+
+// Retrieve fee amount from database using fee schedule ID
+if (isset($_GET['universityID'])) {
     $feeId = $_GET['universityID'];
     $FeeSched = new UniversityFeeSched();
     $FeeSchedData = $FeeSched->showAllDetailsByPayId($feeId);
     foreach($FeeSchedData as $FeeSched) { 
-  $feeamount = $FeeSched['university_amount'];
-  $amount = $feeamount; //since its required then its equavalent.
-  $description = $FeeSched['university_name'];
-  $type = $FeeSched['university_fee_type'];
+        $feeAmount = $FeeSched['university_amount'];
+        $description = $FeeSched['university_name'];
+        $type = $FeeSched['university_fee_type'];
+    }
+} else {
+    echo 'Fee schedule not found.';
+    exit;
 }
-  }else {
-    echo 'hay';
-  }
- 
- // Get other payment details from POST data
+
+if ($paymentAmount >= $feeAmount) {
+    $paymentStatus = 1; // full payment
+    $paymentRemaining = 0;
+} else {
+    $paymentStatus = 0; // partial payment
+    $paymentRemaining = $feeAmount - $paymentAmount;
+}
+
+// Get other payment details from POST data
 $collectedBy = $UserFullname;
 $paymentDate = date('Y-m-d H:i:s');
 $receiptNumber = 'WMSU' . date('YmdHis') . rand(1000, 9999);
@@ -54,18 +68,18 @@ $paymentDetails = [
     'yearlevel' => $year_level,
     'type' => $type,
     'feename' => $description,
-    'amount' => $feeamount,
-    'totalamount' => $amount,
+    'amount' => $feeAmount,
+    'totalamount' => $paymentAmount,
     'collected_by' => $collectedBy,
     'payment_date' => $paymentDate,
+    'payment_status' => $paymentStatus,
+    'payment_remaining' => $paymentRemaining
 ];
 
-$paymentDetails = json_encode($paymentDetails);
 $paymentDetailsJson = json_encode($paymentDetails);
-    if ($payment->savePayment($studentID, $feeScheduleID, $feeamount, $amount, $collectedBy, $paymentDate, $paymentDetailsJson, $receiptNumber)) {
-        header('location: university-complete.php');
-    } else {
-      echo "Failed to save payment.";
-    }
+if ($payment->savePayment($studentID, $feeScheduleID, $feeAmount, $paymentAmount, $collectedBy, $paymentDate, $paymentDetailsJson, $receiptNumber)) {
+    header('location: university-complete.php');
+} else {
+    echo "Failed to save payment.";
+}
 ?>
-
