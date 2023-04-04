2d714ffc2943b23f86ce399ef50a040b02ef011c
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-04-04 18:58:08 +0800

    test

---
+++ new-payment/payment.php
@@ -0,0 +1,77 @@
+<?php
+
+    // resume session here to fetch session values
+    session_start();
+
+	//prevent horny people
+	if (!isset($_SESSION['logged_id'])) {
+		header('location: ../new-payment/payment.php');
+	} else if ($_SESSION['role'] != 'officer') {
+		if ($_SESSION['role'] == 'admin') {
+			header('location: officer.php');
+		} else if ($_SESSION['role'] == 'collector') {
+			header('location: collector.php');
+		}
+	}	
+	require_once '../classes/database.class.php';
+	require_once '../classes/fee.class.php';
+?>
+<!doctype html>
+<html lang="en" class="no-js">
+  <html>
+    <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!--- links for bootstrap and css  --->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/payment-records.css" />
+    <link rel="stylesheet" href="../css/dashboard.css" />
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/6023332cf2.js" crossorigin="anonymous"></script>
+    <title>Wmsu Collection System</title>
+    </head>
+
+      <body>
+      <div class="d-flex" id="wrapper">
+        <!-- Sidebar with bootstrap -->
+       <div class="bg-white" id="sidebar-wrapper">
+            <img src="../images/logo.jpg" width ="200" alt="CCS COLLECTION FEE">
+            <div class="list-group list-group-flush my-3">
+                <a href="../admin/dashboard-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold ">Dashboard</a>
+                <!-- <a href="../fees-user/fees-user.php" class="list-group-item list-group-item-action bg-hover first-text  fw-bold ">Fees</a> -->
+                  <?php
+                  if($_SESSION['role'] == 'officer'){?>
+                <a href="../new-payment/payment.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold active ">Payments</a>
+                    <?php } ?>
+                <a href="../payment-records/payment-records.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold ">Payment Records</a>
+                <a href="../students/students.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold ">Students</a>
+                <a href="../financial-report-user/financial-report-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Financial Report</a>
+                <a href="../audit-log-user/audit-log-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Audit Log</a>
+                <a href="../csc-management/csc-management.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">CSC Management</a>
+                <button class="list-group-item list-group-item-action bg-hover second-text dropdown-btn fw-bold">Admin Settings
+                <i class="fa fa-caret-down" style="margin-left: 37px;"></i>
+                </button>
+                <div class="dropdown-container">
+                    <a href="../admin-settings-user/admin-settings-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold " style="text-decoration:none; padding-left: 70px;">Overview</a>
+                    <a href="../user-univ-fee/univer-fee.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold" style="text-decoration:none; padding-left: 70px;">University Fee</a>
+                    <a href="../user-local-fee/local-fee.php"class="list-group-item list-group-item-action bg-hover first-text fw-bold"  style="text-decoration:none; padding-left: 70px;">Local Fees</a>
+                </div>                
+                <a href="../public/logout.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Logout</a>
+            </div>
+        </div>               
+		<div class="table-responsive">
+	<div id="page-content-wrapper">
+    <nav class="navbar navbar-expand-lg navbar-light bg-active py-4 px-4">
+        <div class="d-flex align-items-center">
+            <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
+            <h2 class="fs-2 m-0">Payments</h2>
+        </div>
+    </nav>
--- students/student-details.php
+++ students/student-details.php
@@ -46,16 +46,10 @@ require_once '../classes/localpayment.class.php';
             <div class="list-group list-group-flush my-3">
                 <a href="../admin/dashboard-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold ">Dashboard</a>
                 <!-- <a href="../fees-user/fees-user.php" class="list-group-item list-group-item-action bg-hover first-text  fw-bold ">Fees</a> -->
-                <button class="list-group-item list-group-item-action bg-hover second-text dropdown-btn fw-bold">Payments</a>
-                <i class="fa fa-caret-down" style = "margin-left:70px;"></i>
-                </button>                
-                <div class="dropdown-container">
                   <?php
                   if($_SESSION['role'] == 'officer'){?>
-                    <a href="../payment/universitypayment.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold " style="text-decoration:none; padding-left: 70px;">University Payment</a>
+                <a href="../new-payment/payment.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold ">Payments</a>
                     <?php } ?>
-                    <a href="../payment-local/localpayment.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold " style="text-decoration:none; padding-left: 70px;">Local Payment</a>
-                </div>
                 <a href="../payment-records/payment-records.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Payment Records</a>
                 <a href="../students/students.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold active">Students</a>
                 <a href="../financial-report-user/financial-report-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Financial Report</a>
