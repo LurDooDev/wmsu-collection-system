cfa64e26a8654e2950468d0e8b2a58634b100b8e
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-04-07 16:56:53 +0800

    tesr

--- new-payment/search-user.php
+++ new-payment/search-user.php
@@ -76,7 +76,7 @@ require_once '../functions/session.function.php';
     <nav class="navbar navbar-expand-lg navbar-light bg-active py-4 px-4">
         <div class="d-flex align-items-center">
             <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-            <h2 class="fs-2 m-0" style="color:#000000; font-weight: 400;">University Payments</h2>
+            <h2 class="fs-2 m-0" style="color:#000000; font-weight: 400;">Payments</h2>
         </div>
     </nav>
     <div class="container">
