e6efae4d3300155c8058c498470b8764045fc914
AndreMickael <111483607+AndreMickael@users.noreply.github.com>
2023-04-13 01:27:02 +0800

    added csc management

--- audit-log-user/audit-log-user.php
+++ audit-log-user/audit-log-user.php
@@ -60,6 +60,7 @@
                 <a href="../students/students.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold  ">Students</a>
                 <a href="../financial-report-user/financial-report-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">Financial Report</a>
                 <a href="../audit-log-user/audit-log-user.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold active">Audit Log</a>
+                 <a href="../csc-management/csc-management.php" class="list-group-item list-group-item-action bg-hover first-text fw-bold">CSC Management</a>
                 <?php
                 if($_SESSION['role'] == 'officer'){?>
                   <button class="list-group-item list-group-item-action bg-hover second-text dropdown-btn fw-bold">Admin Settings
