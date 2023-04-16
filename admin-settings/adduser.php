6deb46f714bf1f5d8d5cee6306f227d0b94aeff2
kent <xt202001241@wmsu.edu.ph>
2023-04-17 00:21:09 +0800

    ok
    fixed and improved

--- admin-settings/adduser.php
+++ admin-settings/adduser.php
@@ -20,50 +20,20 @@ if (isset($_POST['action']) && $_POST['action'] == 'add') {
     $error = array();
     
     if (empty($users->username) || empty($users->userfullname) || empty($users->userposition) || empty($users->roleID) || empty($users->collegeID) || empty($password)) {
-        $error = true; // Set the flag variable to true
-        echo '<script>alert("Please fill in all the required fields");</script>';
-        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
-        return;
+        $error[] = "Please fill in all the required fields";
     }
     
-    else if (strlen($users->username) > 50 || strlen($users->userfullname) > 100 || strlen($users->userposition) > 50 || strlen($password) > 50) {
-        $error = true; // Set the flag variable to true
-        echo '<script>alert("Field length exceeds the limit");</script>';
-        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
-        return;
-    }
-    
-    else if (!is_numeric($users->roleID) || !is_numeric($users->collegeID)) {
-        $error = true; // Set the flag variable to true
-        echo '<script>alert("Invalid input");</script>';
-        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
-        return;
-    }
-
-    else if (strlen($password) < 6) {
-        $error = true; // Set the flag variable to true
-        echo '<script>alert("Password must be at least 6 characters long");</script>';
-        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
-        return;
-    }
-    
-    else if (!empty($error)) {
-        echo 'Error: ' . implode(', ', $error);
-        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
-        exit;
-    }
-
-    // Add user to database
-    if ($users->addUser()) {
-        echo 'Success';
-        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
-        exit;
-    } else {
-        echo 'Error: Unable to add user';
-        echo '<script>window.location.href = "user.php";</script>'; // Redirect to user.php after displaying the error message
-        exit;
+    if (strlen($users->username) > 50 || strlen($users->userfullname) > 100 || strlen($users->userposition) > 50 || strlen($password) > 50) {
+        $error[] = "Field length exceeds the limit";
     }
 }
-
-
 ?>
+
+<?php 
+if(empty($error)){
+    $users->addUser();
+    header("Location: ../admin-settings/user.php");
+    exit();
+}
+?>
+
