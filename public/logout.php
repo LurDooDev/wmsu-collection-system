5f1bc8293713ac0cd45009e1b032d9cce265009e
LurDooDev <devbryansevilla@gmail.com>
2023-04-28 10:50:11 +0800

    a

--- public/logout.php
+++ public/logout.php
@@ -2,6 +2,24 @@
 
     //resume session
     session_start();
+
+    // Retrieve the login time for the current session from the audit trail table
+$fullName = $_SESSION['fullname'];
+$sql = "SELECT login_time FROM audit_trail WHERE full_name = :fullName ORDER BY id DESC LIMIT 1";
+$stmt = $db->connect()->prepare($sql);
+$stmt->bindParam(':fullName', $fullName);
+$stmt->execute();
+$loginTime = $stmt->fetchColumn();
+
+// Store the logout time in the audit trail table
+$logoutTime = date('Y-m-d H:i:s');
+$sql = "UPDATE audit_trail SET logout_time = :logoutTime WHERE full_name = :fullName AND login_time = :loginTime";
+$stmt = $db->connect()->prepare($sql);
+$stmt->bindParam(':fullName', $fullName);
+$stmt->bindParam(':loginTime', $loginTime);
+$stmt->bindParam(':logoutTime', $logoutTime);
+$stmt->execute();
+
     //destroy session
     session_destroy();
     //then send user to login page
